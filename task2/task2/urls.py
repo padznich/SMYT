6b560d305da70fe0802b11b3b7c784f6312671c6
padznich <slabkopg@gmail.com>
2016-12-01 20:40:01 +0300

    [ADD] angjs simple controller

--- task2/task2/urls.py
+++ task2/task2/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from main_app import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^home/$', views.show_home_page),
+    url(r'^home/$', TemplateView.as_view(template_name='home.html')),
+    # url(r'^home/$', views.show_home_page),
 
     # REST
     url(r'^main_app/', include('main_app.urls'))
