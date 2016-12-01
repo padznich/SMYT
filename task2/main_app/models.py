cbb05d30c737b295b2705901b943648266b953c8
padznich <slabkopg@gmail.com>
2016-12-01 19:43:02 +0300

    [REF] verbose_name_plural. def __unicode__

--- task2/main_app/models.py
+++ task2/main_app/models.py
@@ -6,8 +6,17 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField('Category', max_length=64)
 
+    def __unicode__(self):
+        return u"{}".format(self.name)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 
 class Product(models.Model):
     category = models.ForeignKey(Category, verbose_name='Group')
     name = models.CharField('Product name', max_length=128)
     price = models.DecimalField('Unit price', max_digits=10, decimal_places=2)
+
+    def __unicode__(self):
+        return u"{name} {price} {category}".format(name=self.name, price=self.price, category=self.category)
