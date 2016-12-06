2af5f364bb78c256f6c7cd7ccff3d517efccd81f
padznich <slabkopg@gmail.com>
2016-12-06 10:19:33 +0300

    [ADD] unittest2. start tests as "$ nosetests"

--- task2/main_app/tests.py
+++ task2/main_app/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import unittest2
 
-# Create your tests here.
+
+def add(a, b):
+    # return a - b
+    return a + b
+
+
+class AddTest(unittest2.TestCase):
+
+    def test_add(self):
+        self.assertEquals(add(1, 1), 2)
+        self.assertEquals(add(5, 2), 7)
+        self.assertEquals(add(-1, -6), -7)
+
+if __name__ == '__main__':
+    unittest2.main()
--- task2/task2/settings.py
+++ task2/task2/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_nose',
 
     'main_app',
 ]
@@ -122,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
